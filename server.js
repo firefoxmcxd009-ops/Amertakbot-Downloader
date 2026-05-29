9ba4a74f19ea2cda38473c92c9f9564c5890221c
amertak <firefoxmcxd009@gmail.com>
2026-05-30 06:02:42 +0700

    update server.js

--- server.js
+++ server.js
@@ -191,11 +191,6 @@ bot.on("callback_query", async (query) => {
             );
         }
     }
-    
-    if(query.data === "contact") {
-        await bot.answerCallbackQuery(query.id);
-        await bot.sendMessage(chatId, `[Amertak Network](https://t.me/@Amertak_Network)`)
-    }
 });
 
 // ========================
