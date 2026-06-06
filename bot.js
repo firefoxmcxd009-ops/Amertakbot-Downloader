ae8d7d1eaff4bc89c253eb54b6bedb60aa601735
amertak <firefoxmcxd009@gmail.com>
2026-06-06 18:19:13 +0700

    update

--- bot.js
+++ bot.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 
-const TelegramBot = require("node-telegram-bot-api");
+const TelegramBot =
+  require("node-telegram-bot-api");
 
-const bot = new TelegramBot(
-  process.env.BOT_TOKEN,
-  {
-    polling: true
-  }
-);
+const bot =
+  new TelegramBot(
+    process.env.BOT_TOKEN,
+    {
+      polling: true
+    }
+  );
 
 require("./handlers/start")(bot);
 require("./handlers/download")(bot);
